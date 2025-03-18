7fa0438278778e7016aa98a0cfbfacfa459cb415
Kostas Douzis <kostasdouzis@gmail.com>
2025-03-18 18:43:06 +0200

    first commit

---
+++ src/test/java/com/example/demodockerapp/DemoDockerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demodockerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDockerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
