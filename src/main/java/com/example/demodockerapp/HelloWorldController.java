8cce8e0752417f0a331fe3d20c2f3fb490a072ac
Kostas Douzis <kostasdouzis@gmail.com>
2025-03-24 20:08:51 +0200

    Add request mapping

--- src/main/java/com/example/demodockerapp/HelloWorldController.java
+++ src/main/java/com/example/demodockerapp/HelloWorldController.java
@@ -1,13 +1,15 @@
 package com.example.demodockerapp;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HelloWorldController {
 
 
-    @GetMapping
+    @GetMapping("/hello-world")
     public void helloWorld() {
         System.out.println("Hello World");
     }
